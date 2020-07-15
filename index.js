0e498eeb785b2c4547eca12e12be0caeee8fbde8
Adam-Young <awy6448@gmail.com>
2020-07-14 23:23:12 -0400

    Added middleware and endpoint for post and users. completed stretch endpoints.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+const server = require('./server.js')
+const PORT = 1337;
+
+server.listen(PORT, () => {
+  console.log(`\n *** server started on port. *** ${PORT}`)
+})
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
-
 const server = express();
+const UserRouter = require('./users/userRouter.js')
+const PostRouter = require("./posts/postRouter")
+
+server.use(logger)
+server.use(express.json())
+
+server.use('/api/users', UserRouter)
+server.use('/api/posts', PostRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +15,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  let d = new Date().toString()
+  console.log(`${req.method}, ${req.originalUrl}, at ${d}`)
+  next();
+}
 
 module.exports = server;
