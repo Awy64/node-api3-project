4fa8de677393126d8029bac1a0afa3fd1c24267f
Adam-Young <awy6448@gmail.com>
2020-07-15 22:45:03 -0400

    For heroku

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // code away!
+require('dotenv').config();
+
 const server = require('./server.js')
-const PORT = 1337;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
-  console.log(`\n *** server started on port. *** ${PORT}`)
+  console.log(`\n *** server started on port ${PORT}. *** `)
 })
